b16db8d16b7464b10358823a1db81d38c9e544a6
v131v <liya.dobrenkova@ya.ru>
2023-10-22 16:49:08 +0300

    feat: add 2-1 homework

---
+++ hw2-1/main.cpp
@@ -0,0 +1,18 @@
+#include <math.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    double x;
+    scanf("%lf", &x);
+
+    double p = (2 * cos(x - M_PI / 6) + sqrt(2)) * exp(3 * x);
+    double q = 1 / (2 * log1p(x)) + pow(sin(pow(x, 2)), 2);
+    double y = p / q;
+
+    printf("%lf\n", y);
+
+    return 0;
+}
