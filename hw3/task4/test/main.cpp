1774411c7d302dd6975a1511b9f0b0b1b431b406
v131v <liya.dobrenkova@ya.ru>
2023-12-03 13:54:51 +0300

    test: add hw 3-4 test

--- hw3/task4/test/main.cpp
+++ hw3/task4/test/main.cpp
@@ -1,9 +1,42 @@
-#include <iostream>
+#include "../print_binary.h"
 
-using namespace std;
+#include <gtest/gtest.h>
 
-int main()
+TEST(GetBinaryTest, PositiveNumbers) {
+    char result1[65] = ""; // предполагаем максимальную длину бинарной строки
+    getBinary(10, result1);
+    EXPECT_STREQ("0000000000000000000000000000000000000000000000000000000000001010", result1);
+
+    char result2[65] = "";
+    getBinary(27, result2);
+    EXPECT_STREQ("0000000000000000000000000000000000000000000000000000000000011011", result2);
+}
+
+TEST(GetBinaryTest, NegativeNumbers) {
+    char result1[65] = "";
+    getBinary(-15, result1);
+    EXPECT_STREQ("1111111111111111111111111111111111111111111111111111111111110001", result1);
+
+    char result2[65] = "";
+    getBinary(-8, result2);
+    EXPECT_STREQ("1111111111111111111111111111111111111111111111111111111111111000", result2);
+}
+
+TEST(GetBinaryTest, Zero) {
+    char result[64] = "";
+    getBinary(0, result);
+    EXPECT_STREQ("0000000000000000000000000000000000000000000000000000000000000000", result);
+}
+
+// Тест для больших чисел
+TEST(GetBinaryTest, LargeNumbers) {
+    char result[256] = ""; // предполагаем большую длину для больших чисел
+    getBinary(123456789012345, result);
+    EXPECT_STREQ("0000000000000000011100000100100010000110000011011101111101111001", result);
+}
+
+int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
